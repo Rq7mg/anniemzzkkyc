23f8ee2452918a09ea57ab5716cdbb6cdab31dd9
Kyc <163336518+Rq7mg@users.noreply.github.com>
2026-02-18 00:12:45 +0300

    Update skip.py

--- AnnieXMedia/plugins/admins/skip.py
+++ AnnieXMedia/plugins/admins/skip.py
@@ -15,7 +15,7 @@ from config import BANNED_USERS
 
 
 @app.on_message(
-    filters.command(["skip", "cskip", "next", "cnext"], prefixes=["/", "!"]) & filters.group & ~BANNED_USERS
+    filters.command(["skip", "cskip", "atla", "catla"], prefixes=["/", "!"]) & filters.group & ~BANNED_USERS
 )
 @AdminRightsCheck
 async def skip(cli, message: Message, _, chat_id):
