5c19a0d0ef87697d0d929cda729e461178dd05fc
Kyc <163336518+Rq7mg@users.noreply.github.com>
2026-02-18 10:15:49 +0300

    Update active.py

--- AnnieXMedia/plugins/tools/active.py
+++ AnnieXMedia/plugins/tools/active.py
@@ -1,4 +1,4 @@
-﻿# Authored By Certified Coders © 2025
+# Authored By Certified Coders © 2025
 from pyrogram import filters, Client
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from unidecode import unidecode
@@ -14,7 +14,7 @@ from AnnieXMedia.utils.database import (
 
 @app.on_message(filters.command(["activevc", "activevoice", "vc"]) & SUDOERS)
 async def activevc(_, message: Message):
-    mystic = await message.reply_text("» ɢᴇᴛᴛɪɴɢ ᴀᴄᴛɪᴠᴇ ᴠᴏɪᴄᴇ ᴄʜᴀᴛs ʟɪsᴛ...")
+    mystic = await message.reply_text("» Sesli mevzulara bakıyom gardaş, bekle...")
     served_chats = await get_active_chats()
     text = ""
     j = 0
@@ -28,16 +28,16 @@ async def activevc(_, message: Message):
         except:
             await remove_active_chat(x)
     if not text:
-        await mystic.edit_text(f"» ɴᴏ ᴀᴄᴛɪᴠᴇ ᴠᴏɪᴄᴇ ᴄʜᴀᴛs ᴏɴ {app.mention}.")
+        await mystic.edit_text(f"» Şu an hiçbir mekanda sesimiz çıkmıyor la {app.mention}.")
     else:
         await mystic.edit_text(
-            f"<b>» ʟɪsᴛ ᴏғ ᴄᴜʀʀᴇɴᴛʟʏ ᴀᴄᴛɪᴠᴇ ᴠᴏɪᴄᴇ ᴄʜᴀᴛs :</b>\n\n{text}",
+            f"<b>» Şu an öten mekanların listesi :</b>\n\n{text}",
             disable_web_page_preview=True,
         )
 
 @app.on_message(filters.command(["activev", "activevideo", "avc"]) & SUDOERS)
 async def activevi_(_, message: Message):
-    mystic = await message.reply_text("» ɢᴇᴛᴛɪɴɢ ᴀᴄᴛɪᴠᴇ ᴠɪᴅᴇᴏ ᴄʜᴀᴛs ʟɪsᴛ...")
+    mystic = await message.reply_text("» Görüntülü mevzuları süzüyom, iki dakka bekle...")
     served_chats = await get_active_video_chats()
     text = ""
     j = 0
@@ -51,10 +51,10 @@ async def activevi_(_, message: Message):
         except:
             await remove_active_video_chat(x)
     if not text:
-        await mystic.edit_text(f"» ɴᴏ ᴀᴄᴛɪᴠᴇ ᴠɪᴅᴇᴏ ᴄʜᴀᴛs ᴏɴ {app.mention}.")
+        await mystic.edit_text(f"» Hiçbir yerde görüntü patlatmıyoruz la {app.mention}.")
     else:
         await mystic.edit_text(
-            f"<b>» ʟɪsᴛ ᴏғ ᴄᴜʀʀᴇɴᴛʟʏ ᴀᴄᴛɪᴠᴇ ᴠɪᴅᴇᴏ ᴄʜᴀᴛs :</b>\n\n{text}",
+            f"<b>» Şu an ekranda olduğumuz mekanlar :</b>\n\n{text}",
             disable_web_page_preview=True,
         )
 
@@ -63,8 +63,8 @@ async def active_count(client: Client, message: Message):
     ac_audio = str(len(await get_active_chats()))
     ac_video = str(len(await get_active_video_chats()))
     await message.reply_text(
-        f"✫ <b><u>ᴀᴄᴛɪᴠᴇ ᴄʜᴀᴛs ɪɴғᴏ</u></b> :\n\nᴠᴏɪᴄᴇ : {ac_audio}\nᴠɪᴅᴇᴏ  : {ac_video}",
+        f"✫ <b><u>Aktif Mevzuların Durumu</u></b> :\n\nSesli : {ac_audio}\nGörüntülü  : {ac_video}",
         reply_markup=InlineKeyboardMarkup(
-            [[InlineKeyboardButton("✯ ᴄʟᴏsᴇ ✯", callback_data="close")]]
+            [[InlineKeyboardButton("✯ Kapat La ✯", callback_data="close")]]
         )
     )
