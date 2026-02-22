9c8037517491c712b3f3db350b734e7733f7eadc
Kyc <163336518+Rq7mg@users.noreply.github.com>
2026-02-22 08:21:37 +0300

    Update stream.py

--- AnnieXMedia/utils/stream/stream.py
+++ AnnieXMedia/utils/stream/stream.py
@@ -1,18 +1,15 @@
 # Authored By Certified Coders © 2025
 import os
-import aiohttp
-from random import randint
 from typing import Union
 from pyrogram.types import InlineKeyboardMarkup
 import config
-from AnnieXMedia import Carbon, YouTube, app
+from AnnieXMedia import YouTube, app
 from AnnieXMedia.core.call import StreamController
 from AnnieXMedia.misc import db
-from AnnieXMedia.utils.database import add_active_video_chat, is_active_chat
+from AnnieXMedia.utils.database import is_active_chat
 from AnnieXMedia.utils.exceptions import AssistantErr
-from AnnieXMedia.utils.inline import aq_markup, close_markup, stream_markup
-from AnnieXMedia.utils.pastebin import ANNIEBIN
-from AnnieXMedia.utils.stream.queue import put_queue, put_queue_index
+from AnnieXMedia.utils.inline import aq_markup, stream_markup
+from AnnieXMedia.utils.stream.queue import put_queue
 from AnnieXMedia.utils.thumbnails import get_thumb
 from AnnieXMedia.utils.errors import capture_internal_err
 from AnnieXMedia.utils.stream.api_handler import get_video_from_api
@@ -23,36 +20,22 @@ async def stream(
     video: Union[bool, str] = None, streamtype: Union[bool, str] = None,
     spotify: Union[bool, str] = None, forceplay: Union[bool, str] = None,
 ) -> None:
-    if not result:
-        return
-    forceplay = bool(forceplay)
+    if not result: return
     is_video = bool(video)
-    if forceplay:
-        await StreamController.force_stop_stream(chat_id)
+    if bool(forceplay): await StreamController.force_stop_stream(chat_id)
 
     if streamtype == "youtube":
         vidid = result["vidid"]
-        title = (result["title"]).title()
-        duration_min = result["duration_min"]
-        thumbnail = result["thumb"]
-
         file_path = await get_video_from_api(vidid)
         if not file_path:
-            raise AssistantErr("Görüntü kaynağı yok gardaş! API'ler cevap vermiyor. Bi daha oynat de hele.")
+            raise AssistantErr("Görüntü kaynağı yok gardaş! API'ler cevap vermiyor.")
 
         if await is_active_chat(chat_id):
-            await put_queue(chat_id, original_chat_id, file_path, title, duration_min, user_name, vidid, user_id, "video" if is_video else "audio")
-            position = len(db.get(chat_id)) - 1
-            button = aq_markup(_, chat_id)
-            await app.send_message(chat_id=original_chat_id, text=_["queue_4"].format(position, title[:27], duration_min, user_name), reply_markup=InlineKeyboardMarkup(button))
+            await put_queue(chat_id, original_chat_id, file_path, result["title"], result["duration_min"], user_name, vidid, user_id, "video" if is_video else "audio")
+            await app.send_message(chat_id=original_chat_id, text=_["queue_4"].format("Sırada", result["title"][:27], result["duration_min"], user_name))
         else:
-            if not forceplay:
-                db[chat_id] = []
-            await StreamController.join_call(chat_id, original_chat_id, file_path, video=is_video, image=thumbnail)
-            await put_queue(chat_id, original_chat_id, file_path, title, duration_min, user_name, vidid, user_id, "video" if is_video else "audio", forceplay=forceplay)
+            db[chat_id] = []
+            await StreamController.join_call(chat_id, original_chat_id, file_path, video=is_video, image=result["thumb"])
+            await put_queue(chat_id, original_chat_id, file_path, result["title"], result["duration_min"], user_name, vidid, user_id, "video" if is_video else "audio")
             img = await get_thumb(vidid)
-            button = stream_markup(_, chat_id)
-            run = await app.send_photo(original_chat_id, photo=img, caption=_["stream_1"].format(f"https://t.me/{app.username}?start=info_{vidid}", title[:23], duration_min, user_name), reply_markup=InlineKeyboardMarkup(button))
-            db[chat_id][0]["mystic"] = run
-            db[chat_id][0]["markup"] = "stream"
-    # Playlist ve diğerleri get_video_from_api üzerinden çalışacak şekilde devam eder...
+            await app.send_photo(original_chat_id, photo=img, caption=_["stream_1"].format(f"https://t.me/{app.username}?start=info_{vidid}", result["title"][:23], result["duration_min"], user_name), reply_markup=InlineKeyboardMarkup(stream_markup(_, chat_id)))
