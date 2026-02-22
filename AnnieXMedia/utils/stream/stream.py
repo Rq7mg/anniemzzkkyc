9c55bd56668e13ff47e4db629fa97c875ddd4ba8
Kyc <163336518+Rq7mg@users.noreply.github.com>
2026-02-22 09:04:03 +0300

    Update stream.py

--- AnnieXMedia/utils/stream/stream.py
+++ AnnieXMedia/utils/stream/stream.py
@@ -1,57 +1,443 @@
-# Authored By Certified Coders © 2025
+# Bu Mekanın Sahibi Sertifikalı Kodlayıcılar Gardaş © 2025
 import os
+import aiohttp
+import logging
 from random import randint
 from typing import Union
+
 from pyrogram.types import InlineKeyboardMarkup
+
 import config
 from AnnieXMedia import Carbon, YouTube, app
 from AnnieXMedia.core.call import StreamController
 from AnnieXMedia.misc import db
-from AnnieXMedia.utils.database import is_active_chat
+from AnnieXMedia.utils.database import add_active_video_chat, is_active_chat
 from AnnieXMedia.utils.exceptions import AssistantErr
 from AnnieXMedia.utils.inline import aq_markup, close_markup, stream_markup
 from AnnieXMedia.utils.pastebin import ANNIEBIN
-from AnnieXMedia.utils.stream.queue import put_queue
+from AnnieXMedia.utils.stream.queue import put_queue, put_queue_index
 from AnnieXMedia.utils.thumbnails import get_thumb
 from AnnieXMedia.utils.errors import capture_internal_err
 
 @capture_internal_err
 async def stream(
-    _, mystic, user_id, result, chat_id, user_name, original_chat_id,
-    video: Union[bool, str] = None, streamtype: Union[bool, str] = None,
-    spotify: Union[bool, str] = None, forceplay: Union[bool, str] = None,
+    _,
+    mystic,
+    user_id,
+    result,
+    chat_id,
+    user_name,
+    original_chat_id,
+    video: Union[bool, str] = None,
+    streamtype: Union[bool, str] = None,
+    spotify: Union[bool, str] = None,
+    forceplay: Union[bool, str] = None,
 ) -> None:
+    """Mevzuyu başlatan ana motor burası gardaş, dağılın!"""
     if not result:
         return
+
+    forceplay = bool(forceplay)
     is_video = bool(video)
-    if bool(forceplay):
+
+    if forceplay:
         await StreamController.force_stop_stream(chat_id)
 
-    if streamtype == "youtube":
+    if streamtype == "playlist":
+        msg = "Listeyi yüklüyoruz bebe, bekleme yapma!\n\n"
+        count = 0
+        position = 0
+
+        for search in result:
+            if int(count) == config.PLAYLIST_FETCH_LIMIT:
+                continue
+            try:
+                title, duration_min, duration_sec, thumbnail, vidid = await YouTube.details(
+                    search, videoid=search
+                )
+            except Exception:
+                continue
+
+            if str(duration_min) == "None":
+                continue
+            if duration_sec and duration_sec > config.DURATION_LIMIT:
+                continue
+
+            if await is_active_chat(chat_id):
+                await put_queue(
+                    chat_id,
+                    original_chat_id,
+                    f"vid_{vidid}",
+                    title,
+                    duration_min,
+                    user_name,
+                    vidid,
+                    user_id,
+                    "video" if is_video else "audio",
+                )
+                position = len(db.get(chat_id)) - 1
+                count += 1
+                msg += f"{count}. {title[:70]}\n"
+                msg += f"Sırası: {position}\n\n"
+            else:
+                if not forceplay:
+                    db[chat_id] = []
+                
+                try:
+                    file_path, direct = await YouTube.download(
+                        vidid, mystic, video=is_video, videoid=vidid
+                    )
+                except Exception:
+                    continue
+
+                if not file_path:
+                    continue
+
+                await StreamController.join_call(
+                    chat_id,
+                    original_chat_id,
+                    file_path,
+                    video=is_video,
+                    image=thumbnail,
+                )
+                await put_queue(
+                    chat_id,
+                    original_chat_id,
+                    file_path if direct else f"vid_{vidid}",
+                    title,
+                    duration_min,
+                    user_name,
+                    vidid,
+                    user_id,
+                    "video" if is_video else "audio",
+                    forceplay=forceplay,
+                )
+                img = await get_thumb(vidid)
+                button = stream_markup(_, chat_id)
+                run = await app.send_photo(
+                    original_chat_id,
+                    photo=img,
+                    caption=_["stream_1"].format(
+                        f"https://t.me/{app.username}?start=info_{vidid}",
+                        title[:23],
+                        duration_min,
+                        user_name,
+                    ),
+                    reply_markup=InlineKeyboardMarkup(button),
+                )
+                db[chat_id][0]["mystic"] = run
+                db[chat_id][0]["markup"] = "stream"
+
+        if count == 0:
+            return
+        link = await ANNIEBIN(msg)
+        return await app.send_photo(
+            original_chat_id,
+            photo=config.PLAYLIST_IMG_URL,
+            caption=f"Mevzu derin gardaş, {count} parça listeye eklendi.\nLink: {link}",
+            reply_markup=close_markup(_),
+        )
+
+    elif streamtype == "youtube":
         vidid = result["vidid"]
         title = (result["title"]).title()
-        
-        # Senin ilk başta verdiğin orijinal indirme sistemi
+        duration_min = result["duration_min"]
+        thumbnail = result["thumb"]
+
         try:
             file_path, direct = await YouTube.download(
                 vidid, mystic, video=is_video, videoid=vidid
             )
         except Exception:
-            raise AssistantErr(_["play_14"])
-
+            raise AssistantErr("La bebe görüntü mörüntü yok, YouTube fişi çekmiş!")
+            
         if not file_path:
-            raise AssistantErr(_["play_14"])
+            raise AssistantErr("Görüntü kaynağı patlamış gardaş, boşuna debelenme!")
 
         if await is_active_chat(chat_id):
-            await put_queue(chat_id, original_chat_id, file_path if direct else f"vid_{vidid}", title, result["duration_min"], user_name, vidid, user_id, "video" if is_video else "audio")
+            await put_queue(
+                chat_id,
+                original_chat_id,
+                file_path if direct else f"vid_{vidid}",
+                title,
+                duration_min,
+                user_name,
+                vidid,
+                user_id,
+                "video" if is_video else "audio",
+            )
             position = len(db.get(chat_id)) - 1
-            await app.send_message(chat_id=original_chat_id, text=_["queue_4"].format(position, title[:27], result["duration_min"], user_name))
+            button = aq_markup(_, chat_id)
+            await app.send_message(
+                chat_id=original_chat_id,
+                text=f"Sıraya aldık bebe! Pozisyon: {position}\nBaşlık: {title[:27]}",
+                reply_markup=InlineKeyboardMarkup(button),
+            )
         else:
-            if not bool(forceplay):
+            if not forceplay:
                 db[chat_id] = []
-            await StreamController.join_call(chat_id, original_chat_id, file_path, video=is_video, image=result["thumb"])
-            await put_queue(chat_id, original_chat_id, file_path if direct else f"vid_{vidid}", title, result["duration_min"], user_name, vidid, user_id, "video" if is_video else "audio")
+            await StreamController.join_call(
+                chat_id,
+                original_chat_id,
+                file_path,
+                video=is_video,
+                image=thumbnail,
+            )
+            await put_queue(
+                chat_id,
+                original_chat_id,
+                file_path if direct else f"vid_{vidid}",
+                title,
+                duration_min,
+                user_name,
+                vidid,
+                user_id,
+                "video" if is_video else "audio",
+                forceplay=forceplay,
+            )
             img = await get_thumb(vidid)
-            run = await app.send_photo(original_chat_id, photo=img, caption=_["stream_1"].format(f"https://t.me/{app.username}?start=info_{vidid}", title[:23], result["duration_min"], user_name), reply_markup=InlineKeyboardMarkup(stream_markup(_, chat_id)))
+            button = stream_markup(_, chat_id)
+            run = await app.send_photo(
+                original_chat_id,
+                photo=img,
+                caption=_["stream_1"].format(
+                    f"https://t.me/{app.username}?start=info_{vidid}",
+                    title[:23],
+                    duration_min,
+                    user_name,
+                ),
+                reply_markup=InlineKeyboardMarkup(button),
+            )
             db[chat_id][0]["mystic"] = run
             db[chat_id][0]["markup"] = "stream"
+
+    elif streamtype == "soundcloud":
+        file_path = result["filepath"]
+        title = result["title"]
+        duration_min = result["duration_min"]
+        if not file_path:
+            raise AssistantErr("SoundCloud mevzusu patladı, linkte hayır yok!")
+
+        if await is_active_chat(chat_id):
+            await put_queue(
+                chat_id,
+                original_chat_id,
+                file_path,
+                title,
+                duration_min,
+                user_name,
+                streamtype,
+                user_id,
+                "audio",
+            )
+            position = len(db.get(chat_id)) - 1
+            button = aq_markup(_, chat_id)
+            await app.send_message(
+                chat_id=original_chat_id,
+                text=f"SoundCloud sıraya girdi bebe! Sıra: {position}",
+                reply_markup=InlineKeyboardMarkup(button),
+            )
+        else:
+            if not forceplay:
+                db[chat_id] = []
+            await StreamController.join_call(chat_id, original_chat_id, file_path, video=False)
+            await put_queue(
+                chat_id,
+                original_chat_id,
+                file_path,
+                title,
+                duration_min,
+                user_name,
+                streamtype,
+                user_id,
+                "audio",
+                forceplay=forceplay,
+            )
+            button = stream_markup(_, chat_id)
+            run = await app.send_photo(
+                original_chat_id,
+                photo=config.SOUNCLOUD_IMG_URL,
+                caption=_["stream_1"].format(
+                    config.SUPPORT_CHAT, title[:23], duration_min, user_name
+                ),
+                reply_markup=InlineKeyboardMarkup(button),
+            )
+            db[chat_id][0]["mystic"] = run
+            db[chat_id][0]["markup"] = "tg"
+
+    elif streamtype == "telegram":
+        file_path = result["path"]
+        link = result["link"]
+        title = (result["title"]).title()
+        duration_min = result["dur"]
+        if not file_path:
+            raise AssistantErr("Telegram'daki dosya bozuk çıktı gardaş!")
+
+        if await is_active_chat(chat_id):
+            await put_queue(
+                chat_id,
+                original_chat_id,
+                file_path,
+                title,
+                duration_min,
+                user_name,
+                streamtype,
+                user_id,
+                "video" if is_video else "audio",
+            )
+            position = len(db.get(chat_id)) - 1
+            button = aq_markup(_, chat_id)
+            await app.send_message(
+                chat_id=original_chat_id,
+                text=f"Telegram dosyası sıraya girdi! Pozisyon: {position}",
+                reply_markup=InlineKeyboardMarkup(button),
+            )
+        else:
+            if not forceplay:
+                db[chat_id] = []
+            await StreamController.join_call(chat_id, original_chat_id, file_path, video=is_video)
+            await put_queue(
+                chat_id,
+                original_chat_id,
+                file_path,
+                title,
+                duration_min,
+                user_name,
+                streamtype,
+                user_id,
+                "video" if is_video else "audio",
+                forceplay=forceplay,
+            )
+            if is_video:
+                await add_active_video_chat(chat_id)
+            button = stream_markup(_, chat_id)
+            run = await app.send_photo(
+                original_chat_id,
+                photo=config.TELEGRAM_VIDEO_URL if is_video else config.TELEGRAM_AUDIO_URL,
+                caption=_["stream_1"].format(link, title[:23], duration_min, user_name),
+                reply_markup=InlineKeyboardMarkup(button),
+            )
+            db[chat_id][0]["mystic"] = run
+            db[chat_id][0]["markup"] = "tg"
+
+    elif streamtype == "live":
+        link = result["link"]
+        vidid = result["vidid"]
+        title = (result["title"]).title()
+        thumbnail = result["thumb"]
+        duration_min = "BiTMEYEN YAYIN"
+
+        if await is_active_chat(chat_id):
+            await put_queue(
+                chat_id,
+                original_chat_id,
+                f"live_{vidid}",
+                title,
+                duration_min,
+                user_name,
+                vidid,
+                user_id,
+                "video" if is_video else "audio",
+            )
+            position = len(db.get(chat_id)) - 1
+            button = aq_markup(_, chat_id)
+            await app.send_message(
+                chat_id=original_chat_id,
+                text=f"Canlı yayın sıraya damladı! Sıra: {position}",
+                reply_markup=InlineKeyboardMarkup(button),
+            )
+        else:
+            if not forceplay:
+                db[chat_id] = []
+            
+            n, file_path = await YouTube.video(link)
+            if n == 0:
+                raise AssistantErr("Canlı yayın çekilemedi, fişi çekmişler!")
+
+            await StreamController.join_call(
+                chat_id,
+                original_chat_id,
+                file_path,
+                video=is_video,
+                image=thumbnail or None,
+            )
+            await put_queue(
+                chat_id,
+                original_chat_id,
+                f"live_{vidid}",
+                title,
+                duration_min,
+                user_name,
+                vidid,
+                user_id,
+                "video" if is_video else "audio",
+                forceplay=forceplay,
+            )
+            img = await get_thumb(vidid)
+            button = stream_markup(_, chat_id)
+            run = await app.send_photo(
+                original_chat_id,
+                photo=img,
+                caption=_["stream_1"].format(
+                    f"https://t.me/{app.username}?start=info_{vidid}",
+                    title[:23],
+                    duration_min,
+                    user_name,
+                ),
+                reply_markup=InlineKeyboardMarkup(button),
+            )
+            db[chat_id][0]["mystic"] = run
+            db[chat_id][0]["markup"] = "tg"
+
+    elif streamtype == "index":
+        link = result
+        title = "ʟɪɴᴋᴛᴇɴ ᴄᴀʟıʏᴏʀᴜᴢ ɢᴀʀᴅᴀs"
+        duration_min = "00:00"
+
+        if await is_active_chat(chat_id):
+            await put_queue_index(
+                chat_id,
+                original_chat_id,
+                "index_url",
+                title,
+                duration_min,
+                user_name,
+                link,
+                "video" if is_video else "audio",
+            )
+            position = len(db.get(chat_id)) - 1
+            button = aq_markup(_, chat_id)
+            await mystic.edit_text(
+                text=f"Dizin linki sıraya alındı! Pozisyon: {position}",
+                reply_markup=InlineKeyboardMarkup(button),
+            )
+        else:
+            if not forceplay:
+                db[chat_id] = []
+            await StreamController.join_call(
+                chat_id,
+                original_chat_id,
+                link,
+                video=is_video,
+            )
+            await put_queue_index(
+                chat_id,
+                original_chat_id,
+                "index_url",
+                title,
+                duration_min,
+                user_name,
+                link,
+                "video" if is_video else "audio",
+                forceplay=forceplay,
+            )
+            button = stream_markup(_, chat_id)
+            run = await app.send_photo(
+                original_chat_id,
+                photo=config.STREAM_IMG_URL,
+                caption=f"{user_name} gardaşın linkini çalıyoruz!",
+                reply_markup=InlineKeyboardMarkup(button),
+            )
+            db[chat_id][0]["mystic"] = run
+            db[chat_id][0]["markup"] = "tg"
+            await mystic.delete()
